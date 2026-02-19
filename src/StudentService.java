a6697d298b6510a2bed5e38a4b95feb8c88ab439
ndong_georgette <georgetteanna.ndong@epfedu.fr>
2026-02-19 10:05:05 +0100

    Phase 2: Add Student model and service interfaces

---
+++ src/StudentService.java
@@ -0,0 +1,5 @@
+public class StudentService {
+    public void printList() { }
+    public boolean addStudent(String id, String name) { return false; }
+    public boolean removeStudent(String id) { return false; }
+}
